931b8d6cdb0591031c344d7aae1fc513a7f1e68b
Braden <bradenoptimus@gmail.com>
2024-06-02 15:16:29 -0700

    I think I know what I'm doing now?

--- habits.go
+++ habits.go
@@ -4,12 +4,38 @@ import (
     "fmt"
     "encoding/json"
     "os"
+    "time"
 )
 
-type Foo struct {
-    Name string `json:"nombre"` 
-    Age int `json:"alder"` 
-    Height int `json:"hoejde"`
+/*
+  So what do I want to happen? I want to run `goals` from the command 
+  line, and have the thing prepare a list of ongoing goals for me. 
+
+  I want to be focused on goals. The notion of them being done on a 
+  weekly/daily/monthly basis should be up to me. 
+
+  The first thing a goal needs is whether or not it's going to repeat. 
+  
+  If not, then it just needs a completion date, and whether it's a 
+  number goal, or a checkbox.
+
+  If so, then we need when it repeats until, and for now we'll make it
+  log every day, with only a checkbox option available.
+  
+  All goals should have a name, a description, and a due date. 
+*/
+
+type RepeatGoal struct {
+
+}
+
+type OnceGoal struct {
+
+}
+
+type GoalList struct {
+    OnceGoals []OnceGoal `json:"one_time"` 
+    RepeatGoals []RepeatGoal `json:"repeating"` 
 }
 
 func main() {
@@ -17,6 +43,7 @@ func main() {
     // err := json.Unmarshal(thing, &foo)
     // er := os.WriteFile("./thing.json", b, 0644)
     // var foo Foo
-    // thing, er := os.ReadFile("./.config")
+    thing, er := os.ReadFile("./.config")
 
+    
 }
