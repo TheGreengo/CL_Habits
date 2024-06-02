1285df552e6d2435f445234f5b212d964e2846a6
Braden <bradenoptimus@gmail.com>
2024-06-02 16:03:31 -0700

    I?

--- habits.go
+++ habits.go
@@ -26,24 +26,74 @@ import (
 */
 
 type RepeatGoal struct {
-
+    Name string `json:"name"`
+    Done Time   `json:"done"`
+    WeekDays bool `json:"weekdays"`
+    Interval int `json:"inter"`
+    Days [7]bool `json:"days"`
+    Complete bool `json:"complete"`
 }
 
+func (* RepeatGoal) String string {
+    return "your mam"
+}
+
+/*
+  The once goals should have a description, a target date, and then a 
+  response.
+*/
 type OnceGoal struct {
-
+    Name string `json:"name"`
+    Desc string `json:"desc"`
+    Due Time   `json:"due"`
+    Success bool `json:"success"`
 }
 
+func (* OnceGoal) String string {
+    return "your mem"
+}
+/*
+  The goal list should be track finished and active goals for both 
+  repeating and one time goals.
+*/
 type GoalList struct {
     OnceGoals []OnceGoal `json:"one_time"` 
-    RepeatGoals []RepeatGoal `json:"repeating"` 
+    RepeatGoals []RepeatGoal `json:"repeat"` 
+    DoneOnce []OnceGoal `json:"done_once"` 
+    DoneRepeat []RepeatGoal `json:"done_repeat"` 
+}
+
+func (* GoalList) String string {
+    return "your mom"
 }
 
 func main() {
-    // b, err := json.Marshal(thing)
-    // err := json.Unmarshal(thing, &foo)
-    // er := os.WriteFile("./thing.json", b, 0644)
-    // var foo Foo
-    thing, er := os.ReadFile("./.config")
+    // we read in the data
+    raw, er := os.ReadFile("./.config")
 
+    if (er != nil) {
+        fmt.Println(er)
+    }
+
+    var data GoalList
+    err := json.Unmarshal(raw, &data)
+
+    if (err != nil) {
+        fmt.Println(er)
+    }
+
+    // say hello
+    fmt.Println("Welcome to Braden's silly goal thingy")
     
+    // then we need to print out our current goals
+
+    // then you should have the option to select what goal they would
+    // like to update
+
+    // if they finish one, then it should be moved into the completed 
+    // array
+
+    // 
+    // b, err := json.Marshal(thing)
+    // er := os.WriteFile("./thing.json", b, 0644)
 }
