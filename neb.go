4f453a41d77bf161376f2bba96dc58c354fb60bb
Braden <bradenoptimus@gmail.com>
2024-07-04 22:40:31 -0700

    lasdkjasf

--- neb.go
+++ neb.go
@@ -2,9 +2,21 @@ package main
 
 import (
     "fmt"
-	"flag"
+    "os"
 )
 
 func main() {
-	file_ptr := flag.String("f","", "The name of the file to be encrypted")
+    fmt.Println("Welcome to the contained nebula!")
+    file, err := os.OpenFile(os.ExpandEnv("$HOME/neb/stuff"), os.O_RDWR | os.O_CREATE, 0644)
+
+    if err != nil {
+        fmt.Println("something went wrong with the file")
+        fmt.Println(err)
+        fmt.Println("also you suck lolz")
+        return
+    }
+
+    file.Close()
+
+    fmt.Println("the file opened")
 }
